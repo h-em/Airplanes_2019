b3ba8862757cf3a14a550f0a6ac1672879fa2bf9
emanuel <emy_halip@yahoo.com>
2019-04-18 21:22:32 +0300

    Avioane v1.2 - Am sters lucrurile care in plus in cod

--- src/Avioane.java
+++ src/Avioane.java
@@ -57,18 +57,7 @@ public class Avioane {
 
 
     public Plane locatieAvion() {
-        // switch (x) {
-        //  case 0:
-            /*    Move move1 = new Move(1, 0);
-                Move move2 = new Move(2, 0);
-                Move move3 = new Move(3, 0);
-                Move move4 = new Move(2, 1);
-                Move move5 = new Move(0, 2);
-                Move move6 = new Move(1, 2);
-                Move move7 = new Move(2, 2);
-                Move move8 = new Move(3, 2);
-                Move move9 = new Move(4, 2);
-                Move move10 = new Move(2, 3);*/
+
 
         Move[] move = {
                 //coada
@@ -86,21 +75,13 @@ public class Avioane {
                 //cap
                 new Move(2, 3)
         };
+
+        //in obiectul de tip Move retin linia si colana unde se afla capul avionului
         Move head = new Move(2, 3);
 
         return new Plane(move, head);
     }
-    // }
 
-/*
-    public Move setPlane() {
-        int linia = (int) Math.round(Math.random() * 2);
-        int coloana = (int) Math.round(Math.random() * 2);
-
-        System.out.println(linia + "-" + coloana);
-        Move pozitieAvion = new Move(linia, coloana);
-        return pozitieAvion;
-    }*/
 
     public void makeMove(Move move, char symbol) {
         game[move.getLine()][move.getColumn()] = symbol;
@@ -127,7 +108,7 @@ public class Avioane {
         boolean esteAvariat = false;
         //Player currentPlayer = player1;
         //char currentSymbol = SIMBOL_X;
-        int nrAtacuri = 0;
+
         boolean isWin = false;
         while (!isWin) {
 
@@ -139,24 +120,22 @@ public class Avioane {
                     && move.getColumn() >= 0 && move.getColumn() < GAME_SIZE
                     && game[move.getLine()][move.getColumn()] == '*') {
 
-                //verific daca am nimerit avionul
-
+                //iterez prin vectorul de mutari
                 for (int i = 0; i < plane.avion.length; i++) {
 
                     //daca nimeresc o parte din avion
                     if (plane.avion[i].getLine() == move.getLine() && plane.avion[i].getColumn() == move.getColumn()) {
-
+                        //daca nimeresc head-ul  avionul este distrus
                         if ((move.getLine() == plane.head.getLine() && move.getColumn() == plane.head.getColumn())) {
                             System.out.println("Avionul a fost distrus!");
-                            //modific matricea si arat avionul complet
+                            //modific matricea si arat avionul complet distrus
                             arataAvionComplet(plane, SIMBOL_O); //arataAvion(plane.avion[i], SIMBOL_A);
                             esteAvariat = true;
                             isWin = true;
                             break;
                         } else {
-                            //cresc nr atacuri
+                            //daca nimeresc o parte din avion
                             esteAvariat = true;
-                            nrAtacuri++;
                             System.out.println("Avionul a fost Avariat!");
                             //modific maricea
                             arataAvion_oMutare(plane.avion[i], SIMBOL_AVARIAT);
@@ -164,23 +143,21 @@ public class Avioane {
                     }
 
                 }
-                    //afisez doar cand am terminat de iterat prin array-ul meu cu mutari
-                    if (esteAvariat==false) {
-                        System.out.println("Soo close! Try again!");
-                        //inregisterz mutarea in marice
-                        makeMove(move, SIMBOL_X);
-                        showGame();
-                    }
+                //daca nu am nimerit avionul
+                if (esteAvariat == false) {
+                    System.out.println("Soo close! Try again!");
+                    //inregisterz mutarea in marice
+                    makeMove(move, SIMBOL_X);
+                    showGame();
+                }
 
                 esteAvariat = false;
 
-
-
-        } else{
-            System.out.println("Ati introdus o locatie invalida!");
+            } else {
+                System.out.println("Ati introdus o locatie invalida!");
+            }
         }
     }
-}
 
 
 }
